a15ce28f4b3eca5b5a02fa5426bb9342cda66579
therahul413 <therahul413@gmail.com>
2024-08-05 20:58:21 +0530

    5thAug2024

---
+++ src/Aug2024/IntegerValues.java
@@ -0,0 +1,32 @@
+package Aug2024;
+
+public class IntegerValues {
+	
+	
+	public static void main(String[] args) {
+		
+		Integer num1=127;
+		Integer num2=200;
+		Integer num3=127;
+		Integer num4=200;
+		
+		if(num1==num3)
+		{
+			System.out.println("passed");
+		}
+		else
+		{
+			System.out.println("num1 != num2");
+		}
+		
+		if(num2==num4)
+		{
+			System.out.println("passed");
+		}
+		else
+		{
+			System.out.println("num2 != num4");
+		}
+	} 
+
+}
