6d395029086f26a3ea11c383fefd927512c62300
therahul413 <therahul413@gmail.com>
2024-09-05 21:36:36 +0530

    Playing with Array

--- src/Aug2024/IntegerValues.java
+++ src/Aug2024/IntegerValues.java
@@ -3,12 +3,18 @@ package Aug2024;
 public class IntegerValues {
 	
 	
+	/*
+	 * Java caches Integer objects within the range of -128 to 127. 
+	 * This means that for any Integer object created within this range, 
+	 * the JVM returns a reference to an existing object rather than creating a new one. 
+	 * This caching is done to save memory and improve performance.
+	 */
 	public static void main(String[] args) {
 		
 		Integer num1=127;
-		Integer num2=200;
+		Integer num2=126;
 		Integer num3=127;
-		Integer num4=200;
+		Integer num4=126;
 		
 		if(num1==num3)
 		{
